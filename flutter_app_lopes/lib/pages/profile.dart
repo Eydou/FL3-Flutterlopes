308cbe4d9e519ea5de06ce3e433efd66890b4eba
Edouard Touch <edouard.touch@epitech.eu>
2023-04-07 01:38:31 +0200

    [ADD] profil delete recipe

--- flutter_app_lopes/lib/pages/profile.dart
+++ flutter_app_lopes/lib/pages/profile.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_lopes/pages/createRecipe.dart';
+import 'package:flutter_app_lopes/pages/recipe.dart';
 
 import '../class/recipe.dart';
 
@@ -17,12 +19,102 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   final _firebaseAuth = FirebaseAuth.instance;
+
+  Widget buildRowItemsGrid(BuildContext context, Recipe recipe, int index) {
+    return Container(
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => RecipePage(recipe: recipe)),
+            );
+          },
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            elevation: 5.0,
+            margin: EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(15), topRight: Radius.circular(15)),
+                  child: Image.network(
+                      "https://res.cloudinary.com/hv9ssmzrz/image/fetch/c_fill,f_auto,h_600,q_auto,w_800/https://s3-eu-west-1.amazonaws.com/images-ca-1-0-1-eu/tag_photos/original/2426/plat_principal_flickr_2304576748_f79c906b85_b.jpg",
+                      height: 100,
+                      width: double.infinity,
+                      fit: BoxFit.cover),
+                ),
+                Column(
+                  children: [
+                    Container(
+                      child: Text(recipe.name,
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                          maxLines: 2, overflow: TextOverflow.ellipsis),
+                      margin: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 0),
+                    ),
+                    SizedBox(height: 30,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        FittedBox(
+                          fit: BoxFit.fitWidth,
+                          child: Text("P: ${recipe.people.toString()}",
+                              maxLines: 2, overflow: TextOverflow.ellipsis),
+                        ),
+                        FittedBox(
+                          fit: BoxFit.fitWidth,
+                          child: Text("${recipe.time.toString()} min",
+                              maxLines: 2, overflow: TextOverflow.ellipsis),
+                        ),
+                      ],
+                    ),
+                    Align(
+                        alignment: Alignment.centerRight,
+                      child:  Padding(padding: EdgeInsets.all(10),
+                      child: SizedBox(
+                          height: 30,
+                          width: 50, child:TextButton(
+                          child:FittedBox(
+                            fit: BoxFit.fitWidth,
+                            child: Text(
+                            'Delete',
+                              style: TextStyle(fontSize: 11),
+                          )),
+                          style: TextButton.styleFrom(
+                            padding: EdgeInsets.zero,
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.redAccent,
+                            shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(10))),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              FirebaseFirestore.instance.collection('recipes')
+                                  .doc(recipe.id) // <-- Doc ID to be deleted.
+                                  .delete() // <-- Delete
+                                  .then((_) => print('Deleted'))
+                                  .catchError((error) => print('Delete failed: $error'));
+                            });
+                          },
+                        )
+                    )))
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
     Stream<List<Recipe>> readRecipes() => FirebaseFirestore.instance.collection("recipes").snapshots().map((snapshot) => snapshot.docs.map((e) => Recipe.fromJson(e.data())).toList());
+
     return Scaffold(
       backgroundColor: Color.fromRGBO(245, 245, 245, 1.0),
       appBar: AppBar(
+        title: Text("Profil"),
         iconTheme: IconThemeData(color: Colors.black87),
         backgroundColor: Colors.transparent,
         elevation: 0.0,
@@ -30,6 +122,7 @@ class _ProfilePageState extends State<ProfilePage> {
       body: StreamBuilder<List<Recipe>>(stream: readRecipes(), builder: (context, snapshot) {
         if (snapshot.hasData) {
           final recipe = snapshot.data!;
+          final filteredRecipes = recipe.where((element) => element.userId == _firebaseAuth.currentUser!.uid).toList();
           return Column(
             mainAxisSize: MainAxisSize.max,
             children: [
@@ -37,16 +130,18 @@ class _ProfilePageState extends State<ProfilePage> {
                 padding: EdgeInsetsDirectional.fromSTEB(40, 0, 0, 0),
                 child: ProfilePicture(_firebaseAuth.currentUser!.email),
               ),
-              InformationProfile(context),
-              Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(20, 40, 20, 40),
-                child: Container(
-                  color: Colors.redAccent,
-                  height: 350,
-                  child: ListView(
-                    children: recipe.map(buildRecipe).toList(),
-                  ),
-                ),
+             // InformationProfile(context),
+              SizedBox(height: 20),
+              Expanded(
+                child: GridView.count(
+                    crossAxisCount: 2,
+                    childAspectRatio: 3/4,
+                    scrollDirection: Axis.vertical,
+                    physics: PageScrollPhysics(),
+                    shrinkWrap: true,
+                    children: List.generate(snapshot.data!.length, (index) {
+                      return buildRowItemsGrid(context, filteredRecipes[index], index);
+                    })),
               ),
               SizedBox(
                   height: 40,
@@ -69,7 +164,8 @@ class _ProfilePageState extends State<ProfilePage> {
                         MaterialPageRoute(builder: (context) => CreateRecipe()),
                       );
                     },
-                  ))
+                  )),
+              SizedBox(height: 20,),
             ],
           );
         } if(snapshot.hasError) {
